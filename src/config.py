319513f3c98f4040f9f7317f83de7f024f8f300a
Christian Boin <christianboin@Christians-MacBook-Pro-2.local>
2021-02-28 14:57:49 -0500

    restructured again. azure is annoying me.

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config():
     modelB = 'ModelB'
     model = modelB
     
-    res_dir = os.path.join('..', 'res')
+    res_dir = os.path.join('..', '.outputs')
     param_path = "conf.json"
 
     def __init__(self):
